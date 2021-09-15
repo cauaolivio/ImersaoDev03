d56411062960e41c78ec80020f8d5cd8dc60bfc8
Cauã <caua.olivio@gmail.com>
2021-09-15 15:16:21 -0300

    Aula 02 - Otimização e criação de novas funções

--- aula02/logic.js
+++ aula02/logic.js
@@ -1,19 +1,59 @@
 function convert() {
+    let select = document.getElementById("option").value;
+
+    if (select == "dollar") {
+        convertDollar();
+    } else if (select == "euro") {
+        convertEuro();
+    } else if (select == "poundSterling") {
+        convertPoundSterling();
+    }
+}
+
+
+function convertDollar() {
     clearValues();
 
-    var inputValue = document.getElementById("value").value;
+    let inputValue = document.getElementById("value").value;
 
-    var realValue = parseFloat(inputValue * 5).toFixed(2);
+    let realValue = parseFloat(inputValue * 5).toFixed(2);
 
-    var convertedValueElement = document.getElementById("convertValue");
-    var message = `O resultado em real é R$${realValue}`;
+    let convertedValueElement = document.getElementById("convertValue");
+    let message = `O resultado em real é R$${realValue}`;
 
     convertedValueElement.innerHTML = message;
+}
 
+
+function convertEuro() {
+    clearValues();
+
+    let inputValue = document.getElementById("value").value;
+
+    let realValue = parseFloat(inputValue * 6).toFixed(2);
+
+    let convertedValueElement = document.getElementById("convertValue");
+    let message = `O resultado em real é R$${realValue}`;
+
+    convertedValueElement.innerHTML = message;
+}
+
+function convertPoundSterling() {
+    clearValues();
+
+    let inputValue = document.getElementById("value").value;
+
+    let realValue = parseFloat(inputValue * 7).toFixed(2);
+
+    let convertedValueElement = document.getElementById("convertValue");
+    let message = `O resultado em real é R$${realValue}`;
+
+    convertedValueElement.innerHTML = message;
 }
 
 function clearValues() {
-    var convertedValueElement = document.getElementById("convertValue");
+    let convertedValueElement = document.getElementById("convertValue");
 
     convertedValueElement.innerHTML = "";
 }
+
