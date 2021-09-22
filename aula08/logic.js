4d9da2b6124809235c69f78c4df9bab2349278fa
Cauã <caua.olivio@gmail.com>
2021-09-22 19:02:13 -0300

    Aula 08 - Otimização no Super Trunfo

---
+++ aula08/logic.js
@@ -0,0 +1,130 @@
+let cards = [
+    {
+        name: "Fred",
+        img: "./imgs/fred.png",
+        attributes: {
+            humor: 90,
+            agilidade: 100,
+            defesa: 40
+        }
+    },
+    {
+        name: "Chico",
+        img: "./imgs/chico.png",
+        attributes: {
+            humor: 100,
+            agilidade: 50,
+            defesa: 70
+        }
+    },
+    {
+        name: "Bira",
+        img: "./imgs/bira.png",
+        attributes: {
+            humor: 75,
+            agilidade: 40,
+            defesa: 30
+        }
+    },
+    {
+        name: "Paulo",
+        img: "./imgs/paulo.png",
+        attributes: {
+            humor: 70,
+            agilidade: 80,
+            defesa: 10
+        }
+    },
+    {
+        name: "Danilo",
+        img: "./imgs/danilo.png",
+        attributes: {
+            humor: 77,
+            agilidade: 10,
+            defesa: 70
+        }
+    },
+]
+
+let cardMachine = 0;
+let cardPlayer = 0;
+
+function drawCards() {
+    let cardDrawMachine = parseInt(Math.random() * cards.length);
+    cardMachine = cards[cardDrawMachine];
+
+    let cardDrawPlayer = parseInt(Math.random() * cards.length);
+
+    while (cardDrawMachine == cardDrawPlayer) {
+        cardDrawPlayer = parseInt(Math.random() * cards.length);
+    }
+    cardPlayer = cards[cardDrawPlayer];
+
+    document.getElementById("btnDraw").disabled = true;
+    document.getElementById("btnPlay").disabled = false;
+
+    displayPlayerCard();
+}
+
+function getSelectedAttribute() {
+    let radioAttributes = document.getElementsByName("attribute");
+
+    for (let i = 0; i < radioAttributes.length; i++) {
+        if (radioAttributes[i].checked == true) {
+            return radioAttributes[i].value;
+        }
+    }
+}
+
+function play() {
+    let selectedAttribute = getSelectedAttribute();
+    let divResult = document.getElementById("result");
+
+    if (cardPlayer.attributes[selectedAttribute] > cardMachine.attributes[selectedAttribute]) {
+        htmlResult = "<p class='result-final'>Venceu</p>";
+    } else if (cardPlayer.attributes[selectedAttribute] < cardMachine.attributes[selectedAttribute]) {
+        htmlResult = "<p class='result-final'>Perdeu</p>";
+    } else {
+        htmlResult = "<p class='result-final'>Empatou</p>";
+    }
+
+    divResult.innerHTML = htmlResult;
+
+
+    document.getElementById("btnPlay").disabled = true;
+    displayMachineCard();
+}
+
+function displayPlayerCard() {
+    let divPlayerCard = document.getElementById("card-player");
+    divPlayerCard.style.backgroundImage = `url(${cardPlayer.img})`;
+
+    let frame = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    let tagHmtl = "<div id='options' class='card-status'>"
+
+    let optionsText = "";
+    for (let attribute in cardPlayer.attributes) {
+        optionsText += `<input type="radio" name="attribute" value="${attribute}">` + attribute + " " + cardPlayer.attributes[attribute] + "<br>";
+    }
+
+    let name = `<p class="card-subtitle">${cardPlayer.name}</p>`;
+
+    divPlayerCard.innerHTML = frame + name + tagHmtl + optionsText + "</div>";
+}
+
+function displayMachineCard() {
+    let divMachineCard = document.getElementById("card-machine");
+    divMachineCard.style.backgroundImage = `url(${cardMachine.img})`;
+
+    let frame = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    let tagHmtl = "<div id='options' class='card-status'>"
+
+    let optionsText = "";
+    for (let attribute in cardMachine.attributes) {
+        optionsText += `<p type="text" name = "attribute" value="${attribute}">` + attribute + " " + cardMachine.attributes[attribute] + "</p>";
+    }
+
+    let name = `<p class="card-subtitle">${cardMachine.name}</p>`;
+
+    divMachineCard.innerHTML = frame + name + tagHmtl + optionsText + "</div>";
+}
